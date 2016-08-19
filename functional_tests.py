93a00f46afe8483f9f4a430eb82a63d4683c1713
Taewoo <qkqnrpa@gmail.com>
2016-08-19 17:46:37 +0900

    기능 테스트가 작업 아이템을 추가할 수 있는지 확인

--- functional_tests.py
+++ functional_tests.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)     # 암묵적 대기
+        self.browser.implicitly_wait(3)     # 암묵적 대
 
     # 에러가 발생해도 실행되는 메소드지만 setUp()에서 에러가 발생하면 실행되지 않음.
     def tearDown(self):
@@ -15,6 +16,25 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            '작업 아이템 입력'
+        )
+
+        inputbox.send_keys('공작깃털 사기')
+
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')    # element에 s가 붙어서 복수의 요소가 반환된다.
+        self.assertTrue(
+            any(row.text == '1: 공작깃털 사기' for row in rows),
+        )
+
         self.fail('Finish the test!!')
 
 
