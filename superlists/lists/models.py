67a42d8e340757a7d3fd82d8cceb33bfc762e1ef
Taewoo <qkqnrpa@gmail.com>
2016-08-20 23:56:07 +0900

    List 모델 생성 및 외래키 연결

--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
 
+class List(models.Model):
+    pass
+
+
 class Item(models.Model):
+    list = models.ForeignKey(List, default=None)
     text = models.TextField(default='')
