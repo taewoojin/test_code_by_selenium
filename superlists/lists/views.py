08ef15d052b4718d1b9c0e7bad0d3c0f024a5fa1
Taewoo <qkqnrpa@gmail.com>
2016-08-20 17:24:30 +0900

    새로운 목록을 생성하는 url, test, view 수정 및 리팩토링

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -4,10 +4,6 @@ from lists.models import Item
 
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
-
     return render(request, 'home.html')
 
 
@@ -16,3 +12,8 @@ def view_list(request):
     return render(request, 'list.html', {
         'items': items,
     })
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
