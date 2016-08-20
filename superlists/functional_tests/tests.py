d8af3a85df8013cf301698fa2bc60dc29f3d3c9b
Taewoo <qkqnrpa@gmail.com>
2016-08-20 15:08:17 +0900

    functional_tests를 앱처럼 구현하고 LiveServerTestCase를 이용하도록 수정

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,10 +1,11 @@
 import time
 import unittest
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)     # 암묵적 대
@@ -13,13 +14,15 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    # 헬퍼 메소드('test_'로 시작되지 않으면 테스트되지 않기 때문에 리팩토링할 때 사용된다)
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')  # element에 s가 붙어서 복수의 요소가 반환된다.
         self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get('http://localhost:8000')
+        # 로컬 주소를 하드코딩하는 대신에 live_server_url 메소드가 제공됨.
+        self.browser.get(self.live_server_url)
 
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -41,9 +44,3 @@ class NewVisitorTest(unittest.TestCase):
         self.check_for_row_in_list_table('2. 공작깃털을 이용해서 그물 만들기')
 
         self.fail('Finish the test!!')
-
-
-if __name__ == '__main__':
-    # warnings='ignore'는 테스트 작성시에 발생하는 불필요한 리소스 경고를 제거하기 위함.
-    # unittest.main() 함수 호출로 test 실행자 가동.
-    unittest.main(warnings='ignore')
