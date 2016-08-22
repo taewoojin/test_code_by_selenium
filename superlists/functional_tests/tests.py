f50dadf8a5502cc8d4c22705db3d492fce9f1fa2
Taewoo <qkqnrpa@gmail.com>
2016-08-22 15:27:41 +0900

    레이아웃 및 스타일링을 위한 first FT

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -69,3 +69,23 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertIn('우유 사기', page_text)
 
         self.fail('Finish the test!!')
+
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width']/2,
+            512,
+            delta=10        # 오차가 +- 10픽셀 내로.
+        )
+
+        inputbox.send_keys('testing\n')
+        time.sleep(5)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
