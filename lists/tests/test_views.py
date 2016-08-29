c7eda4b9a727340a5e14ac11ece6dd3b37fa9d1c
Taewoo <qkqnrpa@gmail.com>
2016-08-29 17:21:23 +0900

    list_view 에서 모델 유효성 검증 구현

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -71,6 +71,17 @@ class ListViewTest(TestCase):
         )
         self.assertRedirects(response, '/lists/{0}/'.format(correct_list.id))
 
+    def test_validatoin_errors_end_up_on_lists_page(self):
+        list_ = List.objects.create()
+        response = self.client.post(
+            '/lists/{0}/'.format(list_.id),
+            data={'item_text': ''}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+
 
 class NewListTest(TestCase):
     def test_saving_a_POST_request(self):
--- lists/views.py
+++ lists/views.py
@@ -10,12 +10,20 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    error = None
+
     if request.method == 'POST':
-        item = Item(text=request.POST['item_text'], list=list_)
-        item.save()
-        return redirect('/lists/{0}/'.format(list_id))
+        try:
+            item = Item(text=request.POST['item_text'], list=list_)
+            item.full_clean()
+            item.save()
+            return redirect('/lists/{0}/'.format(list_id))
+        except ValidationError:
+            error = "You can't have an empty list item"
+
     return render(request, 'list.html', {
         'list': list_,
+        'error': error,
     })
 
 
